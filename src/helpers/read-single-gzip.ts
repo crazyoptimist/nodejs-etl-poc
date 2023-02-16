772de4fca9b9112612f6c088cd9c8c2891ec42ce
crazyoptimist <hey@crazyoptimist.net>
2023-02-16 01:15:36 -0600

    refactor: improve logging in single zip read

--- src/helpers/read-single-gzip.ts
+++ src/helpers/read-single-gzip.ts
@@ -2,6 +2,10 @@ import { unzipSync } from "zlib";
 import fs from "fs";
 
 export function readSingleGzip(filePath: string) {
-  const unzipped = unzipSync(fs.readFileSync(filePath)).toString("utf-8");
-  return JSON.parse(unzipped);
+  try {
+    const unzipped = unzipSync(fs.readFileSync(filePath)).toString("utf-8");
+    return JSON.parse(unzipped);
+  } catch (e) {
+    console.error("error while reading single gzip file: ", filePath);
+  }
 }
